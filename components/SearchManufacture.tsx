229263c185681f664558c2441b9aec4b6581c294
Samson Ocran <samson.ocran@gmail.com>
2023-11-28 11:54:18 +0100

    adding cars api

--- components/SearchManufacture.tsx
+++ components/SearchManufacture.tsx
@@ -23,7 +23,7 @@ const SearchManufacture = ({
   return (
     <div className="search-manufacturer">
       <Combobox value={manufacturer} onChange={setManufacturer}>
-        <div className="relative w-full">
+        <div className="relative w-full flex items-center">
           <Combobox.Button className="absolute top=[14px]">
             <Image
               src="/car-logo.svg"
